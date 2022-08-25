0fe7f967562d4432f7a33b07f3337a98cc7e014e
dengyt <2248545525@qq.com>
2022-08-25 17:11:46 +0800

    feat: ui组件

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,48 +1,60 @@
-import React from 'react';
-import './button.css';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Button as AntButton } from 'antd'
 
-interface ButtonProps {
+export const Button: React.FC<any> = ({
+  primary,
+  type,
+  backgroundColor,
+  size,
+  label,
+  danger,
+  ...props
+}) => {
+  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary'
+
+  return (
+    <div className="warpper">
+      <AntButton
+        type={type}
+        size={size}
+        danger={danger}
+        className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+        style={backgroundColor && { backgroundColor }}
+        {...props}
+      >
+        {label}
+      </AntButton>
+    </div>
+  )
+}
+
+Button.propTypes = {
   /**
    * Is this the principal call to action on the page?
    */
-  primary?: boolean;
+  primary: PropTypes.bool,
   /**
    * What background color to use
    */
-  backgroundColor?: string;
+  backgroundColor: PropTypes.string,
   /**
    * How large should the button be?
    */
-  size?: 'small' | 'medium' | 'large';
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
   /**
    * Button contents
    */
-  label: string;
+  label: PropTypes.string.isRequired,
   /**
    * Optional click handler
    */
-  onClick?: () => void;
+  onClick: PropTypes.func
 }
 
-/**
- * Primary UI component for user interaction
- */
-export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
-}: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
-  return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
-      {...props}
-    >
-      {label}
-    </button>
-  );
-};
+Button.defaultProps = {
+  backgroundColor: null,
+  primary: false,
+  size: 'medium',
+  onClick: undefined
+}
